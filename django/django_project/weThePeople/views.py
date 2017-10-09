e179cfd260748f26d0add799e38d5afb565982bd
cmsilv <cmsilv@umich.edu>
2017-10-09 15:27:12 +0000

    cleaned up previous commit

--- django/django_project/weThePeople/views.py
+++ django/django_project/weThePeople/views.py
@@ -6,9 +6,6 @@ def index(request):
 	return render(request, 'weThePeople/index.html')
 # Create your views here
 def testdb(request):
-#	from pymongo import MongoClient
-#	client = MongoClient('localhost', 27017)
-#	test_db = client.test
 	people_collection = test_db.people
 	post = {
 		"name": "Taylor",
@@ -18,14 +15,4 @@ def testdb(request):
 	person = people_collection.find_one({"name": "Taylor"})
 	del person['_id']
 	people_collection.delete_many({})
-#	json_person = JSONEncoder().encode(person)
-
-
-#	people_collection = test_db.people
-#	person = {"name": "Taylor",
-#		"favorite_saying": "Curt is my HIRO"
-#	}
-#	people_collection.insert_one(person)
-#	document = people_collection.find_one()
-#	people_collection.delete_many({"name": "Taylor"})
 	return JsonResponse(person)
